bfa37ee55d7a093227e15724bee3bb269601e862
Robby <90345430+nf-36@users.noreply.github.com>
2025-08-02 23:38:21 -0500

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,5 +1,5 @@
 const rateLimit = 30000;
-const rateLimitDuration = 5 * 60 * 1000; // 5 minutes
+const rateLimitDuration = 1 * 60 * 1000; // 5 minutes
 const requestCounts = new Map();
 
 const corsHeaders = {
